b3f8368ce00c2fbb35af49ec820f23bcb4b26b96
DanielMolinaS01 <daniel.molina01@correo.usa.edu.co>
2023-09-06 01:00:54 -0500

    Implementacion del metodo Builder

---
+++ model/builder/ConcreteBuilder.java
@@ -0,0 +1,83 @@
+public class ConcreteBuilder implements Builder {
+
+    private BodyGenderAttributeFactory bodyGenderAttributeFactory = new BodyGenderAttributeFactory();
+    private BodySkinAttributeFactory bodySkinAttributeFactory = new BodySkinAttributeFactory();
+    private BodyAttributeEnum bodyGenderAttribute;
+    private BodyAttributeEnum bodySkinAttribute;
+    private BodyPart head;
+    private BodyPart pants;
+    private BodyPart shoes;
+    private BodyPart torso;
+
+    public BodyAttributeEnum getBodyGenderAttribute() {
+        return bodyGenderAttribute;
+    }
+
+    public BodyAttributeEnum getBodySkinAttribute() {
+        return bodySkinAttribute;
+    }
+
+    public BodyPart getPants() {
+        return pants;
+    }
+
+    public BodyPart getShoes() {
+        return shoes;
+    }
+
+    public BodyPart getTorso() {
+        return torso;
+    }
+
+    public BodyPart getHead() {
+        return head;
+    }
+
+    public FunkoPopModel() {
+        
+    }
+
+
+    public void updateBody(BodyAttributeEnum bodyAttributeGender,
+    BodyAttributeEnum bodyAttributeSkin,
+    int headNumber,
+    int pantsNumber,
+    int shoesNumber,
+    int torsoNumber
+    )
+    {
+        this.head = BodyPartFactory.getBodyPart(BodyType.HEAD,
+            bodyGenderAttributeFactory.getBodyAttribute(bodyAttributeGender),
+            bodySkinAttributeFactory.getBodyAttribute(bodyAttributeSkin),
+            headNumber
+        );
+
+        this.pants = BodyPartFactory.getBodyPart(BodyType.PANTS,
+            bodyGenderAttributeFactory.getBodyAttribute(bodyAttributeGender),
+            bodySkinAttributeFactory.getBodyAttribute(bodyAttributeSkin),
+            pantsNumber
+        );
+
+        this.shoes = BodyPartFactory.getBodyPart(BodyType.SHOES,
+            bodyGenderAttributeFactory.getBodyAttribute(bodyAttributeGender),
+            bodySkinAttributeFactory.getBodyAttribute(bodyAttributeSkin),
+            shoesNumber
+        );
+
+        this.torso = BodyPartFactory.getBodyPart(BodyType.TORSO,
+            bodyGenderAttributeFactory.getBodyAttribute(bodyAttributeGender),
+            bodySkinAttributeFactory.getBodyAttribute(bodyAttributeSkin),
+            torsoNumber
+        );
+
+        this.bodyGenderAttribute = bodyAttributeGender;
+        this.bodySkinAttribute = bodyAttributeSkin;
+    }
+
+    // Implementa otros métodos para configurar atributos
+
+    @Override
+    public Product build() {
+        return new Product(attribute1, attribute2);
+    }
+}
